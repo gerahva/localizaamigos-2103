3963934241fb1970396cf0e9729bc005b2de6972
daton <rapidclimate@gmail.com>
2021-06-25 19:37:03 -0500

    BackEnd actualizado 15 de junio de 2021

--- src/test/java/com/unitec/localizaamigos/LocalizaamigosApplicationTests.java
+++ src/test/java/com/unitec/localizaamigos/LocalizaamigosApplicationTests.java
@@ -3,11 +3,18 @@ package com.unitec.localizaamigos;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class LocalizaamigosApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+@Test
+	void addition_isCorrect() {
+		assertEquals(5, 2 + 3);
 	}
 
 }
